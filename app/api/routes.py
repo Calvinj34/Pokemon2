736cb5b9948c7c7e6c44e5ba4b82808298c15792
Calvin Johnson <kingcalvin22@yahoo.com>
2023-02-02 19:49:29 -0700

    final

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request
-# from ..models import Post
 # from .apiauth import basic_auth_required, token_auth_required
 # from .apiauth import basic_auth_required, token_auth_required, basic_auth
 
--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
-
 db = SQLAlchemy()
 
 
